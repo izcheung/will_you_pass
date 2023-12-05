aef909421b5695ecd7d7ae5ea13422106f99001d
IRENE CHEUNG <icheung24@my.bcit.ca>
2023-12-05 00:59:59 -0800

    Add test cases for move_character

---
+++ test_move_character.py
@@ -0,0 +1,44 @@
+
+from unittest import TestCase
+from game import move_character
+
+
+class TestMoveplayer(TestCase):
+
+    def test_move_character_north(self):
+        player = {'name': 'Irene', 'level': 1, 'intelligence': 0, 'HP': 10, 'position': [3, 1], 'location': 'tech_hub',
+                  'chocolate': 0}
+        direction = "1"
+        move_character(player, direction)
+        expected = [3, 0]
+        actual = player["position"]
+        self.assertEqual(expected, actual)
+
+    def test_move_character_south(self):
+        player = {'name': 'Irene', 'level': 1, 'intelligence': 0, 'HP': 10, 'position': [3, 1], 'location': 'tech_hub',
+                  'chocolate': 0}
+        direction = "2"
+        move_character(player, direction)
+        expected = [3, 2]
+        actual = player["position"]
+        self.assertEqual(expected, actual)
+
+    def test_move_character_east(self):
+        player = {'name': 'Irene', 'level': 1, 'intelligence': 0, 'HP': 10, 'position': [3, 1], 'location': 'tech_hub',
+                  'chocolate': 0}
+        direction = "3"
+        move_character(player, direction)
+        expected = [4, 1]
+        actual = player["position"]
+        self.assertEqual(expected, actual)
+
+    def test_move_character_west(self):
+        player = {'name': 'Irene', 'level': 1, 'intelligence': 0, 'HP': 10, 'position': [3, 1], 'location': 'tech_hub',
+                  'chocolate': 0}
+        direction = "4"
+        move_character(player, direction)
+        expected = [2, 1]
+        actual = player["position"]
+        self.assertEqual(expected, actual)
+
+

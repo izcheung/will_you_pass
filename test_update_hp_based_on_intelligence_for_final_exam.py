22eceba3bace50f86e5e066ec08479b81239ed1f
IRENE CHEUNG <icheung24@my.bcit.ca>
2023-12-05 00:46:42 -0800

    Add test cases for update_hp_based_on_intelligence

---
+++ test_update_hp_based_on_intelligence_for_final_exam.py
@@ -0,0 +1,53 @@
+from unittest import TestCase
+from game import update_HP_based_on_intelligence_for_final_exam
+
+
+class TestUpdateHPBasedOnIntelligenceForFinalExam(TestCase):
+
+    def test_update_hp_based_on_intelligence_for_final_exam_at_0(self):
+        player = {'name': 'Irene', 'level': 1, 'intelligence': 0, 'HP': 10, 'position': [3, 1], 'location': 'tech_hub', \
+         'chocolate': 0}
+        update_HP_based_on_intelligence_for_final_exam(player)
+        actual = player['HP']
+        expected = 1
+        self.assertEqual(expected, actual)
+
+    def test_update_hp_based_on_intelligence_for_final_exam_below_500(self):
+        player = {'name': 'Irene', 'level': 1, 'intelligence': 50, 'HP': 10, 'position': [3, 1], 'location': 'tech_hub', \
+         'chocolate': 0}
+        update_HP_based_on_intelligence_for_final_exam(player)
+        actual = player['HP']
+        expected = 1
+        self.assertEqual(expected, actual)
+
+    def test_update_hp_based_on_intelligence_for_final_exam_at_500(self):
+        player = {'name': 'Irene', 'level': 1, 'intelligence': 500, 'HP': 10, 'position': [3, 1], 'location': 'tech_hub', \
+         'chocolate': 0}
+        update_HP_based_on_intelligence_for_final_exam(player)
+        actual = player['HP']
+        expected = 2
+        self.assertEqual(expected, actual)
+
+    def test_update_hp_based_on_intelligence_for_final_exam_between_500_and_1000(self):
+        player = {'name': 'Irene', 'level': 1, 'intelligence': 749, 'HP': 10, 'position': [3, 1], 'location': 'tech_hub', \
+         'chocolate': 0}
+        update_HP_based_on_intelligence_for_final_exam(player)
+        actual = player['HP']
+        expected = 2
+        self.assertEqual(expected, actual)
+
+    def test_update_hp_based_on_intelligence_for_final_exam_at_1000(self):
+        player = {'name': 'Irene', 'level': 1, 'intelligence': 1000, 'HP': 10, 'position': [3, 1], 'location': 'tech_hub', \
+         'chocolate': 0}
+        update_HP_based_on_intelligence_for_final_exam(player)
+        actual = player['HP']
+        expected = 3
+        self.assertEqual(expected, actual)
+
+    def test_update_hp_based_on_intelligence_for_final_exam_over_1000(self):
+        player = {'name': 'Irene', 'level': 1, 'intelligence': 1500, 'HP': 10, 'position': [3, 1], 'location': 'tech_hub', \
+         'chocolate': 0}
+        update_HP_based_on_intelligence_for_final_exam(player)
+        actual = player['HP']
+        expected = 3
+        self.assertEqual(expected, actual)

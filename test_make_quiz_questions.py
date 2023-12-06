8ac2e5ede6a31593b95b9993570393fe3a16ad06
IRENE CHEUNG <icheung24@my.bcit.ca>
2023-12-06 10:35:55 -0800

    Change class name

--- test_make_quiz_questions.py
+++ test_make_quiz_questions.py
@@ -1,7 +1,7 @@
 from unittest import TestCase
 from game import make_quiz_questions
 
-class Test(TestCase):
+class TestMakeQuizQuestions(TestCase):
     def test_make_quiz_questions(self):
         expected = {"Is the dress blue/black or white/gold?": 2, "Are hot dogs sandwiches?": 4,
                     "Why are pizza boxes square but pizzas are circular?": 3,

b28df052543fd35b1c38f55942dbe0036bab734b
IRENE CHEUNG <icheung24@my.bcit.ca>
2023-12-06 10:36:26 -0800

    Fix format warning

--- test_check_level.py
+++ test_check_level.py
@@ -15,6 +15,7 @@ class TestCheckLevel(TestCase):
         actual = check_level(player, surprise_questions)
         expected = None
         self.assertEqual(expected, actual)
+
     @patch('random.sample',
            side_effect=lambda population, k: [(0, 6)] if k == 1 else [(0, 6), (0, 0), (4, 6), (4, 2), (2, 6)])
     def test_check_level_up_to_2(self, _):
@@ -31,9 +32,9 @@ class TestCheckLevel(TestCase):
         player = {'name': 'Irene', 'level': 2, 'intelligence': 0, 'HP': 10, 'position': [3, 1],
                   'location': 'student_lounge',
                   'chocolate': 7}
-        surprise_questions = {"Is the dress blue/black or white/gold?": 2, "Are hot dogs sandwiches?": 4,
-                              "Why are pizza boxes square but pizzas are circular?": 3,
-                              "Is the opposite of opposite the same or opposite": 2}
+        surprise_questions = {"Is the dress blue/black or white/gold?": 4, "Are hot dogs sandwiches?": 8,
+                              "Why are pizza boxes square but pizzas are circular?": 6,
+                              "Is the opposite of opposite the same or opposite": 4}
         actual = check_level(player, surprise_questions)
         expected = None
         self.assertEqual(expected, actual)

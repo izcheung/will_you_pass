e5269eb2e166c3d38e98af72337c22eedad07442
IRENE CHEUNG <icheung24@my.bcit.ca>
2023-12-09 17:48:47 -0800

    Shorten code length

--- unit_tests/test_make_character.py
+++ unit_tests/test_make_character.py
@@ -10,13 +10,15 @@ class TestMakeCharacter(TestCase):
         self.assertEqual(expected, actual)
 
     def test_make_character_all_symbols(self):
-        expected = {'name': '@!#$%$', 'level': 1, 'intelligence': 0, 'HP': 10, 'position': [3, 1], 'location': 'tech_hub',
+        expected = {'name': '@!#$%$', 'level': 1, 'intelligence': 0, 'HP': 10, 'position': [3, 1], 'location':
+            'tech_hub',
                     'chocolate': 0}
         actual = make_character('@!#$%$')
         self.assertEqual(expected, actual)
 
     def test_make_character_all_numbers(self):
-        expected = {'name': '12345', 'level': 1, 'intelligence': 0, 'HP': 10, 'position': [3, 1], 'location': 'tech_hub',
+        expected = {'name': '12345', 'level': 1, 'intelligence': 0, 'HP': 10, 'position': [3, 1], 'location':
+            'tech_hub',
                     'chocolate': 0}
         actual = make_character('12345')
         self.assertEqual(expected, actual)

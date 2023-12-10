4ab87db74f24e64b35d05ec8b4aaeb0ff4664ae7
IRENE CHEUNG <icheung24@my.bcit.ca>
2023-12-09 21:30:10 -0800

    Fix surprise question damage points

--- unit_tests/test_level_up_to_2.py
+++ unit_tests/test_level_up_to_2.py
@@ -42,15 +42,14 @@ class TestLevelUpTo2(TestCase):
             'Because why not?': 3,
             'Is the opposite of opposite the same or opposite\nChoices:\n1: Same\n2: Opposite\n3: Opposite opposite\n4'
             ': Opposite squared': 2}
-        level_up_to_2(player, surprise_questions)
-        actual = surprise_questions
+        actual = level_up_to_2(player, surprise_questions)
         expected = {
-            'Is the dress blue/black or white/gold?\nChoices:\n1: Blue/black\n2: White/gold:\n3: Neither\n4: Both': 2,
+            'Is the dress blue/black or white/gold?\nChoices:\n1: Blue/black\n2: White/gold:\n3: Neither\n4: Both': 4,
             'Are hot dogs sandwiches?\nChoices:\n1: Yes\n2: No, they are tacos.\n3: No, they cannot be classified as '
-            'anything other than hot dogs.\n4: Maybe, it depends on my mood': 4,
+            'anything other than hot dogs.\n4: Maybe, it depends on my mood': 8,
             'Why are pizza boxes square but pizzas are circular?\nChoices:\n1: Because it is cheaper\n2: Because it'
             ' is easier to take out a slice when the box is square.\n3: Because nothing makes sense in the world.\n4: '
-            'Because why not?': 3,
+            'Because why not?': 6,
             'Is the opposite of opposite the same or opposite\nChoices:\n1: Same\n2: Opposite\n3: Opposite opposite\n4:'
-            ' Opposite squared': 2}
+            ' Opposite squared': 4}
         self.assertEqual(expected, actual)
--- unit_tests/test_level_up_to_3.py
+++ unit_tests/test_level_up_to_3.py
@@ -7,39 +7,38 @@ from unittest.mock import patch
 class TestLevelUpTo3(TestCase):
     @patch('sys.stdout', new_callable=io.StringIO)
     def test_level_up_to_3_printed(self, mock_output):
-        player = {'name': 'Irene', 'level': 2, 'intelligence': 100, 'HP': 10, 'position': [3, 1], 'location': 'student_lounge',
-                  'chocolate': 10}
+        player = {'name': 'Irene', 'level': 2, 'intelligence': 100, 'HP': 10, 'position': [3, 1],
+                  'location': 'student_lounge', 'chocolate': 10}
         surprise_questions = {"Is the dress blue/black or white/gold?": 4, "Are hot dogs sandwiches?": 8,
                               "Why are pizza boxes square but pizzas are circular?": 6,
                               "Is the opposite of opposite the same or opposite": 4}
         level_up_to_3(player, surprise_questions)
         printed_output = mock_output.getvalue()
-        expected = "YOU LEVELED UP TO LEVEL 3! (Stats - HP: 20 (+10), Intelligence: 300 (+200)) You have now advanced to the Room 645. CAUTION: Some of the quizzes here can kill you in one shot. Your task now is to find Chris and give him the chocolates.\n"
+        expected = ("YOU LEVELED UP TO LEVEL 3! (Stats - HP: 20 (+10), Intelligence: 300 (+200)) You have now advanced "
+                    "to the Room 645. CAUTION: Some of the quizzes here can kill you in one shot. Your task now is to"
+                    " find Chris and give him the chocolates.\n")
         self.assertEqual(expected, printed_output)
 
     def test_level_up_to_3_character_stats(self):
-        player = {'name': 'Irene', 'level': 2, 'intelligence': 100, 'HP': 10, 'position': [3, 1], 'location': 'student_lounge',
-                  'chocolate': 10}
+        player = {'name': 'Irene', 'level': 2, 'intelligence': 100, 'HP': 10, 'position': [3, 1],
+                  'location': 'student_lounge', 'chocolate': 10}
         surprise_questions = {"Is the dress blue/black or white/gold?": 4, "Are hot dogs sandwiches?": 8,
                               "Why are pizza boxes square but pizzas are circular?": 6,
                               "Is the opposite of opposite the same or opposite": 4}
         level_up_to_3(player, surprise_questions)
         actual = player
         expected = {'name': 'Irene', 'level': 3, 'intelligence': 300, 'HP': 20, 'position': [1, 2],
-                    'location': 'room_645',
-                    'chocolate': 10}
+                    'location': 'room_645', 'chocolate': 10}
         self.assertEqual(expected, actual)
 
     def test_level_up_to_3_surprise_question_stats(self):
-        player = {'name': 'Irene', 'level': 2, 'intelligence': 100, 'HP': 10, 'position': [3, 1], 'location': 'student_lounge',
-                  'chocolate': 10}
+        player = {'name': 'Irene', 'level': 2, 'intelligence': 100, 'HP': 10, 'position': [3, 1],
+                  'location': 'student_lounge', 'chocolate': 10}
         surprise_questions = {"Is the dress blue/black or white/gold?": 4, "Are hot dogs sandwiches?": 8,
                               "Why are pizza boxes square but pizzas are circular?": 6,
                               "Is the opposite of opposite the same or opposite": 4}
-        level_up_to_3(player, surprise_questions)
-        actual = surprise_questions
+        actual = level_up_to_3(player, surprise_questions)
         expected = {"Is the dress blue/black or white/gold?": 8, "Are hot dogs sandwiches?": 16,
-                              "Why are pizza boxes square but pizzas are circular?": 12,
-                              "Is the opposite of opposite the same or opposite": 8}
+                    "Why are pizza boxes square but pizzas are circular?": 12,
+                    "Is the opposite of opposite the same or opposite": 8}
         self.assertEqual(expected, actual)
-

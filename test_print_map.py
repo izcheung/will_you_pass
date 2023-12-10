79b24c8215f3715914051ffc59435d1358a85f81
IRENE CHEUNG <icheung24@my.bcit.ca>
2023-12-09 18:32:51 -0800

    Add test cases for print_map functions

--- test_print_map.py
+++ test_print_map.py
@@ -8,64 +8,66 @@ from game import print_map
 
 class TestPrintMap(TestCase):
 
-    @patch('random.sample',
-           side_effect=lambda population, k: [(1, 3)] if k == 1 else [(1, 3), (2, 5), (3, 3), (4, 2), (2, 3)])
     @patch('sys.stdout', new_callable=io.StringIO)
-    def test_print_map_tech_hub(self, mock_output, _):
+    def test_print_map_tech_hub(self, mock_output):
         player = {'name': 'Irene', 'level': 1, 'intelligence': 0, 'HP': 10, 'position': [3, 1], 'location': 'tech_hub',
                   'chocolate': 0}
-        area_description = {
-            'rows': 8,
-            'columns': 7,
-            'obstacles': [(1, 1), (2, 1), (1, 2), (2, 2), (6, 2), (5, 3), (6, 3), (1, 4), (6, 4), (1, 5),
-                          (1, 6), (1, 7), (2, 7), (3, 7), (4, 7)],
-            'Chris': None
-        }
-        add_map_boundaries(area_description)
-        chocolate_coordinates = [(1, 3), (2, 5), (3, 3), (4, 2), (2, 3)]
-        print_map(player, area_description, chocolate_coordinates)
-        expected = "#######\n###*  #\n### ! #\n#!!! ##\n##    #\n##!   #\n##    #\n#######\nYou are at position [3, 1]\n\n"
+        area_description = {'rows': 8, 'columns': 7, 'obstacles': [(1, 1), (2, 1), (1, 2), (2, 2), (6, 2), (5, 3),
+                                                                   (6, 3), (1, 4), (6, 4), (1, 5), (1, 6), (1, 7),
+                                                                   (2, 7), (3, 7), (4, 7), (0, 0), (1, 0), (2, 0),
+                                                                   (3, 0), (4, 0), (5, 0), (6, 0), (0, 7), (1, 7),
+                                                                   (2, 7), (3, 7), (4, 7), (5, 7), (6, 7), (0, 1),
+                                                                   (0, 2), (0, 3), (0, 4), (0, 5), (0, 6), (6, 1),
+                                                                   (6, 5), (6, 6)], 'Chris': None,
+                            'chocolate_coordinates': [(4, 5), (4, 1), (4, 4), (2, 4), (2, 6)]}
+        print_map(player, area_description)
+        expected = ("#######\n###*! #\n###   #\n#    ##\n##! ! #\n##  ! #\n##!   #\n#######\n"
+                    "You are at position [3, 1]\n\n")
         actual = mock_output.getvalue()
         self.assertEqual(expected, actual)
 
-    @patch('random.sample',
-           side_effect=lambda population, k: [(1, 2)] if k == 1 else [(1, 2), (2, 6), (3, 3), (4, 2), (2, 3)])
     @patch('sys.stdout', new_callable=io.StringIO)
-    def test_print_map_student_lounge(self, mock_output, _):
-        player = {'name': 'Irene', 'level': 3, 'intelligence': 0, 'HP': 10, 'position': [3, 1],
-                  'location': 'room_645',
-                  'chocolate': 10}
-        area_description = {
-            'rows': 4,
-            'columns': 12,
-            'obstacles': [(1, 1), (2, 1), (3, 1), (7, 1), (8, 1), (10, 2), (1, 3), (10, 3)],
-            'Chris': (6, 1)
-        }
-        add_map_boundaries(area_description)
-        chocolate_coordinates = [(1, 2), (2, 6), (3, 3), (4, 2), (2, 3)]
-        print_map(player, area_description, chocolate_coordinates)
-        expected = "############\n###*  C##  #\n#!  !     ##\n############\nYou are at position [3, 1]\n\n"
-        actual = mock_output.getvalue()
-        self.assertEqual(expected, actual)
-
-    @patch('random.sample',
-           side_effect=lambda population, k: [(1, 3)] if k == 1 else [(1, 3), (2, 6), (3, 3), (4, 2), (2, 3)])
-    @patch('sys.stdout', new_callable=io.StringIO)
-    def test_print_map_student_lounge(self, mock_output, _):
+    def test_print_map_room_645(self, mock_output):
         player = {'name': 'Irene', 'level': 1, 'intelligence': 0, 'HP': 10, 'position': [3, 1],
-                  'location': 'student_lounge',
-                  'chocolate': 5}
-        area_description = {
-            'rows': 10,
-            'columns': 7,
-            'obstacles': [(1, 1), (2, 1), (5, 1), (6, 1), (1, 2), (2, 2), (5, 2), (6, 2), (5, 3), (6, 3),
-                          (5, 4), (6, 4), (1, 5), (2, 5), (1, 6), (2, 6), (1, 7), (2, 7), (5, 7), (6, 7),
-                          (1, 8), (2, 8), (5, 8), (6, 8), (1, 9), (2, 9), (5, 9), (6, 9)],
-            'Chris': None
-        }
-        add_map_boundaries(area_description)
-        chocolate_coordinates = [(1, 3), (2, 6), (3, 3), (4, 2), (2, 3)]
-        print_map(player, area_description, chocolate_coordinates)
-        expected = "#######\n###* ##\n### !##\n#!!! ##\n#    ##\n###   #\n###   #\n###  ##\n###  ##\n#######\nYou are at position [3, 1]\n\n"
+                  'location': 'student_lounge', 'chocolate': 0}
+        area_description = {'rows': 10, 'columns': 7,
+                            'obstacles': [(1, 1), (2, 1), (5, 1), (6, 1), (1, 2), (2, 2), (5, 2), (6, 2), (5, 3),
+                                          (6, 3), (5, 4), (6, 4), (1, 5), (2, 5), (1, 6), (2, 6), (1, 7), (2, 7),
+                                          (5, 7), (6, 7), (1, 8), (2, 8), (5, 8), (6, 8), (1, 9), (2, 9), (5, 9),
+                                          (6, 9), (0, 0), (1, 0), (2, 0), (3, 0), (4, 0), (5, 0), (6, 0), (0, 9),
+                                          (1, 9), (2, 9), (3, 9), (4, 9), (5, 9), (6, 9), (0, 1), (0, 2), (0, 3),
+                                          (0, 4), (0, 5), (0, 6), (0, 7), (0, 8), (6, 5), (6, 6)],
+                            'Chris': None, 'chocolate_coordinates': [(3, 6), (3, 3), (4, 3), (3, 8), (3, 2)]}
+        print_map(player, area_description)
+        expected = ("#######\n\n###* ##\n###! ##\n#  !!##\n#    ##\n###   #\n###!  #\n###  ##\n###! ##\n#######\n"
+                    "You are at position [3, 1]\n\n")
         actual = mock_output.getvalue()
         self.assertEqual(expected, actual)
+
+    @patch('sys.stdout', new_callable=io.StringIO)
+    def test_print_map_room_645(self, mock_output):
+        player = {'name': 'Irene', 'level': 1, 'intelligence': 0, 'HP': 10, 'position': [3, 1], 'location': 'room_645', 'chocolate': 0}
+        area_description = {'rows': 4, 'columns': 12, 'obstacles': [(1, 1), (2, 1), (3, 1), (7, 1), (8, 1), (10, 2), (1, 3), (10, 3), (0, 0), (1, 0), (2, 0), (3, 0), (4, 0), (5, 0), (6, 0), (7, 0), (8, 0), (9, 0), (10, 0), (11, 0), (0, 3), (1, 3), (2, 3), (3, 3), (4, 3), (5, 3), (6, 3), (7, 3), (8, 3), (9, 3), (10, 3), (11, 3), (0, 1), (0, 2), (11, 1), (11, 2)], 'Chris': (6, 1), 'chocolate_coordinates': []}
+        print_map(player, area_description)
+        expected = ("############\n###*  C##  #\n#         ##\n############\n"
+                    "You are at position [3, 1]\n\n")
+        actual = mock_output.getvalue()
+        self.assertEqual(expected, actual)
+
+    @patch('sys.stdout', new_callable=io.StringIO)
+    def test_print_map_student_lounge(self, mock_output):
+        player = {'name': 'Irene', 'level': 1, 'intelligence': 0, 'HP': 10, 'position': [3, 1], 'location': 'room_645',
+                  'chocolate': 0}
+        area_description = {'rows': 4, 'columns': 12,
+                            'obstacles': [(1, 1), (2, 1), (3, 1), (7, 1), (8, 1), (10, 2), (1, 3), (10, 3), (0, 0),
+                                          (1, 0), (2, 0), (3, 0), (4, 0), (5, 0), (6, 0), (7, 0), (8, 0), (9, 0),
+                                          (10, 0), (11, 0), (0, 3), (1, 3), (2, 3), (3, 3), (4, 3), (5, 3), (6, 3),
+                                          (7, 3), (8, 3), (9, 3), (10, 3), (11, 3), (0, 1), (0, 2), (11, 1), (11, 2)],
+                            'Chris': (6, 1), 'chocolate_coordinates': []}
+        print_map(player, area_description)
+        expected = ("############\n###*  C##  #\n#         ##\n############\n"
+                    "You are at position [3, 1]\n\n")
+        actual = mock_output.getvalue()
+        self.assertEqual(expected, actual)
+
+

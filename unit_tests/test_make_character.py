85bd6d43757cad9921c190e5821de50aaefee426
IRENE CHEUNG <icheung24@my.bcit.ca>
2023-12-09 17:49:43 -0800

    Fix syntax error warning

--- unit_tests/test_make_character.py
+++ unit_tests/test_make_character.py
@@ -4,21 +4,21 @@ from game import make_character
 
 class TestMakeCharacter(TestCase):
     def test_make_character_all_letters(self):
-        expected = {'name': 'Irene', 'level': 1, 'intelligence': 0, 'HP': 10, 'position': [3, 1], 'location': 'tech_hub',
+        expected = {'name': 'Irene', 'level': 1, 'intelligence': 0, 'HP': 10, 'position': [3, 1],
+                    'location': 'tech_hub',
                     'chocolate': 0}
         actual = make_character('Irene')
         self.assertEqual(expected, actual)
 
     def test_make_character_all_symbols(self):
         expected = {'name': '@!#$%$', 'level': 1, 'intelligence': 0, 'HP': 10, 'position': [3, 1], 'location':
-            'tech_hub',
-                    'chocolate': 0}
+                    'tech_hub', 'chocolate': 0}
         actual = make_character('@!#$%$')
         self.assertEqual(expected, actual)
 
     def test_make_character_all_numbers(self):
         expected = {'name': '12345', 'level': 1, 'intelligence': 0, 'HP': 10, 'position': [3, 1], 'location':
-            'tech_hub',
+                    'tech_hub',
                     'chocolate': 0}
         actual = make_character('12345')
         self.assertEqual(expected, actual)
